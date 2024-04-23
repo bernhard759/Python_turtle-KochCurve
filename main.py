38f27819944667ac583f893c467b0194718badf3
bernhard759 <67150681+bernhard759@users.noreply.github.com>
2024-04-23 06:49:16 +0000

    change code to draw different orders

--- main.py
+++ main.py
@@ -14,29 +14,43 @@ def koch_curve(t, order, size):
 
 
 # Drawer func
-def draw_koch_curve(order, size):
-  window = turtle.Screen()
-  window.bgcolor("white")
-  window.title("Koch Curve Fractal")
-
+def draw_koch_curve(order, size, x, y, color):
   fractal_turtle = turtle.Turtle()
-  fractal_turtle.color("blue")
   fractal_turtle.penup()
-  fractal_turtle.goto(-size / 2, size / 4)
+  fractal_turtle.goto(x, y)
   fractal_turtle.pendown()
   fractal_turtle.speed(0)
+  fractal_turtle.begin_fill()
+  fractal_turtle.fillcolor(color)
 
-  # Start drawing here
   for _ in range(3):
     koch_curve(fractal_turtle, order, size)
     fractal_turtle.right(120)
 
+  fractal_turtle.end_fill()
+
+
+def draw_grid():
+  window = turtle.Screen()
+  window.bgcolor("white")
+  window.title("Koch Curve Fractal Grid")
+  window_width, window_height = 400, 400
+  window.setup(width=window_width, height=window_height)
+
+  cell_size = 200
+  cell_offset = cell_size / 2
+
+  colors = ["brown", "darkcyan", "AliceBlue", "goldenrod"]
+
+  for i in range(2):
+    for j in range(2):
+      order = 1 + i * 2 + j
+      x = j * cell_size - cell_offset
+      y = i * cell_size - cell_offset
+      print("Drawing with order", order, "at", x, y)
+      draw_koch_curve(order, 150, x, y, colors[i * 2 + j])
+
   window.mainloop()
 
 
-# Adjust order and size to change the complexity and size of the fractal
-order = 4
-size = 300
-
-# Start the turtle
-draw_koch_curve(order, size)
+draw_grid()
